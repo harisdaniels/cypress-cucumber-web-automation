7a77fd9464d179cc1bd15477681dadc39cb46055
Haris Kaisar Daniels <haris.kaisar@mekari.com>
2023-05-29 08:37:27 +0700

    update utilities.js

--- cypress/lib/utilities/utilities.js
+++ cypress/lib/utilities/utilities.js
@@ -1,21 +1,19 @@
-let randomText = "";
 let pattern = "abcdefghijklmnopqrstuvwxyz";
 
 export const generateEmailFormat = () => {
+  let randomText = "";
   let emailFormat = "";
   for (let i = 0; i < 10; i ++) {
       randomText += pattern.charAt(Math.floor(Math.random() * pattern.length));
       emailFormat = randomText + "@gmail.com";
   }
-
   return emailFormat;
 }
 
 export const generateText = () => {
+  let randomText = "";
   for (let i = 0; i < 10; i ++) {
       randomText += pattern.charAt(Math.floor(Math.random() * pattern.length));
-      string = randomText;
   }
-
-  return string;
+  return randomText;
 }
